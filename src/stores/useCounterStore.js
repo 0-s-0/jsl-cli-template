dfb968cfe1805056b94470d2c4f74ea5ecac36ea
0-s-0 <xushicai@proton.me>
2025-04-03 10:49:57 +0200

    安装pinia状态管理

---
+++ src/stores/useCounterStore.js
@@ -0,0 +1,25 @@
+import { defineStore } from 'pinia';
+
+export const useCounterStore = defineStore('counter', {
+  state: () => ({
+    count: 0
+  }),
+  actions: {
+    increment() {
+      this.count++;
+    },
+    decrement() {
+      this.count--;
+    },
+    async incrementAsync() {
+      setTimeout(() => {
+        this.increment();
+      }, 1000);
+    }
+  },
+  getters: {
+    doubleCount(state) {
+      return state.count * 2;
+    }
+  }
+});
